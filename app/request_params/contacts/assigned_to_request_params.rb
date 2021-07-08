173d295eff2e7cc1517bbc72a51e0a741f1e8d8c
quoctuan <quoctuan@DESKTOP-BRQSBKE.localdomain>
2021-07-08 18:04:26 +0700

    [TUGO-12] Tao api get contacts by owner id

---
+++ app/request_params/contacts/assigned_to_request_params.rb
@@ -0,0 +1,13 @@
+module Contacts
+  class AssignedToRequestParams < TugoCommon::RequestParamsBase
+    attribute :owner_id, :integer
+
+    validates :owner_id, numericality: {only_integer: true, greater_than: 0}
+
+    def initialize(params)
+      super(
+        owner_id: params[:owner_id]
+      )
+    end
+  end
+end
